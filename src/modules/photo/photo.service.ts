33c12e6dc1288f7ef10bc5766fae3adcbce8e0a6
Vitor <vit.riber1@hotmail.com>
2022-01-06 19:30:18 -0300

    feat/adicionado configuração do S3

--- src/modules/photo/photo.service.ts
+++ src/modules/photo/photo.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreatePhotoDto } from './dto/create-photo.dto';
 import { Photo } from './models/photo.model';
 import { AlbumService } from '../album/album.service';
+import { S3 } from 'aws-sdk';
 
 @Injectable()
 export class PhotoService {
@@ -31,6 +32,37 @@ export class PhotoService {
 
   async update(id: string, photo: Partial<Photo>) {
     return await this.photoRepository.update(id, photo);
+  }
+
+  async upload(file: Express.Multer.File) {
+    const { originalname } = file;
+    const bucketS3 = 'my-aws-bucket';
+    await this.uploadS3(file.buffer, bucketS3, originalname);
+  }
+
+  async uploadS3(file, bucket, name) {
+    const s3 = this.getS3();
+    const params = {
+      Bucket: bucket,
+      Key: String(name),
+      Body: file,
+    };
+    return new Promise((resolve, reject) => {
+      s3.upload(params, (err, data) => {
+        if (err) {
+          Logger.error(err);
+          reject(err.message);
+        }
+        resolve(data);
+      });
+    });
+  }
+
+  getS3() {
+    return new S3({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    });
   }
 
   async delete(id: string) {
