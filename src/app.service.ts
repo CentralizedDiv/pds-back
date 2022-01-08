73e0ef5e7cda997109d28d754c677d2cf77406e4
Vitor <vit.riber1@hotmail.com>
2022-01-08 18:44:26 -0300

    feat/adicionado rota de enviar email

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import Mail from './services/mail';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!!!';
+  }
+
+  sendMessage(message: any): string {
+    Mail.to = message.to;
+    Mail.subject = message.subject;
+    Mail.message = message.message;
+    const result = Mail.sendMail();
+
+    return result + 'E-mail enviado';
   }
 }
